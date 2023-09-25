4b5b12fd2c1ddd7234ca6a3ac2e021c1fe5e51e7
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2023-09-25 01:12:39 +0100

    tailwind and shacn ui integration

--- frontend/src/usersDirectories/LandingPageDirectory/Pages/LandingHomePage.jsx
+++ frontend/src/usersDirectories/LandingPageDirectory/Pages/LandingHomePage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
 const LandingHomePage = () => {
   return (
     <>
       <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline mx-20">Hello world!</h1>
+      <Button>Click me</Button>
     </>
   );
 };
